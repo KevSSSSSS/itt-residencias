8c8ea2c9cd4af502c2d786b3588788151d9f7fb4
Alex <aa.roockie@gmail.com>
2022-10-24 05:58:42 -0500

    Datepicker Kevs

--- src/screens/AddResident.js
+++ src/screens/AddResident.js
@@ -1,270 +1,391 @@
-import React from "react";
-import { Col, Row, Container, Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import {
+  Col,
+  Row,
+  Container,
+  Form,
+  Button,
+  InputGroup,
+  ButtonGroup,
+  ToggleButton,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
-import {BsFillArrowLeftSquareFill} from "react-icons/bs";
+import {
+  BsFillArrowLeftSquareFill,
+  BsFillPersonPlusFill,
+} from "react-icons/bs";
+import Dropdown from "react-bootstrap/Dropdown";
+
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+//Datos de ejemplo
+import { careers, semesters } from "../test/DataExample";
+
+const radios = [
+  { name: "Hombre", value: "1" },
+  { name: "Mujer", value: "2" },
+];
 
 export default function AddResident() {
+  const [groupSelected, setGroupSelected] = useState("Elige una carrera");
+  const [semester, setSemester] = useState("Elige un semestre");
+  const [period, setPeriod] = useState("Elige un periodo");
+  const [startDateR1, setStartDateR1] = useState(null);
+  const [startDateR2, setStartDateR2] = useState(null);
+  const [startDateR3, setStartDateR3] = useState(null);
+  const [startDateStart, setStartDateStart] = useState(null);
+  const [startDateFinish, setStartDateFinish] = useState(null);
+  const [radioValue, setRadioValue] = useState(1);
+  const [approved, setApproved] = useState(null);
+
   return (
-
-
-
-    <Container style={{marginTop: 10}}>
+    <Container style={{ marginTop: 10 }}>
       <Link to={"/"}>
-      <BsFillArrowLeftSquareFill size={36}/>
+        <BsFillArrowLeftSquareFill size={36} />
       </Link>
-      <h1>Agregar un Residente</h1>
+      {/*Datos del alumno*/}
+      <h1>
+        <BsFillPersonPlusFill size={45}></BsFillPersonPlusFill>
+        Agregar un residente
+      </h1>
+
       <br></br>
-      <h5>Datos del Alumno</h5>
+      <h5>Datos del alumno</h5>
       <Form>
         <Form.Group>
           <Row>
             <Col>
               <Form.Label>Nombre(s)</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <Form.Control type="text"></Form.Control>
             </Col>
             <Col>
-              <Form.Label>Apellido P</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <Form.Label>Apellido Paterno</Form.Label>
+              <Form.Control type="text"></Form.Control>
             </Col>
             <Col>
-              <Form.Label>Apellido M</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
-            </Col>
-            <Col>
-              <Form.Label>Grupo</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
-            </Col>
-            <Col>
-              <Form.Label>Sexo</Form.Label>
+              <Form.Label>Apellido Materno</Form.Label>
               <Form.Control type="text"></Form.Control>
             </Col>
           </Row>
           <Row>
+            <Col>
+              <Row>
+                <Col>
+                  <Form.Label>Carrera</Form.Label>
+                  <Dropdown>
+                    <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                      {groupSelected}
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      {careers.map((i) => {
+                        return (
+                          <Dropdown.Item
+                            key={i}
+                            onClick={() => {
+                              setGroupSelected(i);
+                            }}
+                          >
+                            {i}
+                          </Dropdown.Item>
+                        );
+                      })}
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Col>
+                <Col>
+                  <Form.Label>Semestre</Form.Label>
+                  <Dropdown>
+                    <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                      {semester}
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      {semesters.map((i) => {
+                        return (
+                          <Dropdown.Item
+                            key={i}
+                            onClick={() => {
+                              setSemester(i);
+                            }}
+                          >
+                            {i}
+                          </Dropdown.Item>
+                        );
+                      })}
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Col>
+              </Row>
+            </Col>
             <Col>
               <Form.Label>Número de Control</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
-            </Col>
-            <Col>
-              <Form.Label>Semestre</Form.Label>
               <Form.Control type="text"></Form.Control>
             </Col>
             <Col>
-            
-              <Form.Label>Correo Institucional</Form.Label>
-              <Form.Control
-                 type="text" value="@toluca.tecnm.mx"
-              ></Form.Control>
+              <div>
+                <Form.Label>Sexo</Form.Label>
+              </div>
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? "outline-primary" : "outline-danger"}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Label>Domicilio</Form.Label>
+              <Form.Control type="text"></Form.Control>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Label>Correo institucional</Form.Label>
+              <InputGroup className="mb-3">
+                <Form.Control
+                  placeholder="Alumno"
+                  aria-label="Alumno"
+                  aria-describedby="basic-addon2"
+                />
+                <InputGroup.Text id="basic-addon2">
+                  @toluca.tecnm.mx
+                </InputGroup.Text>
+              </InputGroup>
             </Col>
             <Col>
-              <Form.Label>Correo Electronico 2</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
-            </Col>
-            <Row>
-            <Col>
-              <Form.Label>Correo Electronico 3</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <Form.Label>Correo personal</Form.Label>
+              <Form.Control type="text"></Form.Control>
             </Col>
             <Col>
-              <Form.Label>Correo Electronico 4</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <Form.Label>Correo opcional</Form.Label>
+              <Form.Control type="text"></Form.Control>
             </Col>
-            <Col>
-              <Form.Label>Celular</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
-            </Col>
+          </Row>
+          <Row>
             <Col>
               <Form.Label>Telefono de Casa</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <Form.Control type="text"></Form.Control>
             </Col>
-            
-            </Row>
+            <Col>
+              <Form.Label>Telefono celular</Form.Label>
+              <Form.Control type="text"></Form.Control>
+            </Col>
           </Row>
         </Form.Group>
         <br></br>
-        <h5>Datos del Asesor Externo</h5>
+        <h5>Datos del asesor externo</h5>
         <Form.Group>
           <Row>
             <Col>
               <Form.Label>Nombre(s)</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <Form.Control type="text"></Form.Control>
             </Col>
             <Col>
-              <Form.Label>Apellido P</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <Form.Label>Apellido Paterno</Form.Label>
+              <Form.Control type="text"></Form.Control>
             </Col>
             <Col>
-              <Form.Label>Apellido M</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
-            </Col>
-            <Col>
-              <Form.Label>Correo Electronico</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <Form.Label>Apellido Materno</Form.Label>
+              <Form.Control type="text"></Form.Control>
             </Col>
             <Row>
-            <Col>
-              <Form.Label>Correo Electronico 2</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
-            </Col>
-            <Col>
-              <Form.Label>Telefono</Form.Label>
-              <Form.Control type="text"></Form.Control>
-            </Col>
-            <Col>
-              <Form.Label>Telefono 2</Form.Label>
-              <Form.Control type="text"></Form.Control>
-            </Col>
-            
+              <Col>
+                <Form.Label>Correo electronico</Form.Label>
+                <Form.Control type="text"></Form.Control>
+              </Col>
+              <Col>
+                <Form.Label>Correo electronico 2</Form.Label>
+                <Form.Control type="text"></Form.Control>
+              </Col>
+              <Col>
+                <Form.Label>Telefono</Form.Label>
+                <Form.Control type="text"></Form.Control>
+              </Col>
+              <Col>
+                <Form.Label>Telefono 2</Form.Label>
+                <Form.Control type="text"></Form.Control>
+              </Col>
             </Row>
           </Row>
         </Form.Group>
-        <br></br>
-        <h5>Datos del Asesor Interno</h5>
-        <Form.Group>
-          <Row>
-            <Col>
-              <Form.Label>Nombre(s)</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
-            </Col>
-            <Col>
-              <Form.Label>Apellido P</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
-            </Col>
-            <Col>
-              <Form.Label>Apellido M</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
-            </Col>
-            <Col>
-              <Form.Label>Correo Electronico</Form.Label>
-              <Form.Control
-                type="text" value="@toluca.tecnm.mx"
-              ></Form.Control>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Label>Correo Electronico 2</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
-            </Col>
-            </Row>
-        </Form.Group>
-        <br></br>
-        <h5>Datos del Proyecto</h5>
+        <br />
+        <h5>Datos del proyecto</h5>
         <Form.Group>
           <Row>
             <Col>
               <Form.Label>Nombre del Proyecto</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <Form.Control type="text"></Form.Control>
             </Col>
             <Col>
               <Form.Label>Número de Revisión</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <Form.Control type="text"></Form.Control>
             </Col>
             <Col>
               <Form.Label>Empresa</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <Form.Control type="text"></Form.Control>
             </Col>
             <Col>
               <Form.Label>Periodo</Form.Label>
-              <Form.Control type="text" ></Form.Control>
+              <Dropdown>
+                <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                  {period}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item
+                    onClick={() => {
+                      setPeriod("Enero - Junio");
+                    }}
+                  >
+                    Enero - Junio
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    onClick={() => {
+                      setPeriod("Agosto - Diciembre");
+                    }}
+                  >
+                    Agosto - Diciembre
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </Col>
-            <Col>
+          </Row>
+          <Row>
+            <Col md="auto">
               <Form.Label>Fecha 1 de Revisión</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <DatePicker
+                selected={startDateR1}
+                onChange={(date) => setStartDateR1(date)}
+                placeholderText={"Agregue una fecha"}
+              />
             </Col>
-            </Row>
-            <Row>
-            <Col>
+            <Col md="auto">
               <Form.Label>Fecha 2 de Revisión</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <DatePicker
+                selected={startDateR2}
+                onChange={(date) => setStartDateR2(date)}
+                placeholderText={"Agregue una fecha"}
+              />
             </Col>
-            <Col>
-              <Form.Label>Número de Oficio</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+            <Col md="auto">
+              <Form.Label>Fecha 3 de Revisión</Form.Label>
+              <DatePicker
+                selected={startDateR3}
+                onChange={(date) => setStartDateR3(date)}
+                placeholderText={"Agregue una fecha"}
+              />
             </Col>
-          <Col>
+          </Row>
+          <Row>
+            <Col md="auto">
               <Form.Label>Inicio de Residencia</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <DatePicker
+                selected={startDateStart}
+                onChange={(date) => setStartDateStart(date)}
+                placeholderText={"Agregue una fecha"}
+              />
             </Col>
-            <Col>
+            <Col md="auto">
               <Form.Label>Finalización de Residencia</Form.Label>
-              <Form.Control
-                type="text"
-              ></Form.Control>
+              <DatePicker
+                selected={startDateFinish}
+                onChange={(date) => setStartDateFinish(date)}
+                placeholderText={"Agregue una fecha"}
+              />
             </Col>
           </Row>
         </Form.Group>
         <br></br>
+        <div
+          style={{
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+          }}
+        >
+          <ButtonGroup>
+            <Button
+              variant="outline-success"
+              size="lg"
+              active={approved}
+              onClick={() => {
+                setApproved(true);
+              }}
+            >
+              Aprobar
+            </Button>
+            <Button
+              variant="outline-danger"
+              size="lg"
+              active={approved == false}
+              onClick={() => {
+                setApproved(false);
+              }}
+            >
+              Rechazar
+            </Button>
+          </ButtonGroup>
+        </div>
+        <Col>
+          <Form.Label>Subir archivos</Form.Label>
+          <Form.Control type="file" multiple></Form.Control>
+        </Col>
+        <br></br>
+        {approved && (
+          <>
+            <h5>Datos del asesor interno</h5>
+            <Form.Group>
+              <Row>
+                <Col>
+                  <Form.Label>Nombre(s)</Form.Label>
+                  <Form.Control type="text"></Form.Control>
+                </Col>
+                <Col>
+                  <Form.Label>Apellido Paterno</Form.Label>
+                  <Form.Control type="text"></Form.Control>
+                </Col>
+                <Col>
+                  <Form.Label>Apellido Materno</Form.Label>
+                  <Form.Control type="text"></Form.Control>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Form.Label>Correo electronico</Form.Label>
+                  <Form.Control type="text"></Form.Control>
+                </Col>
+              </Row>
+            </Form.Group>
+            <Row>
+              <Col md="2">
+                <Form.Label>Número de Oficio</Form.Label>
+                <Form.Control type="text"></Form.Control>
+              </Col>
+            </Row>
+          </>
+        )}
+        <br></br>
         <Button
-          variant="success"
+          variant="outline-success"
+          size="lg"
           onClick={() => {
             alert("Se registro un usuario");
           }}
         >
-          Registrar Alumno
+          Registrar
         </Button>
-        
-        <Col>
-        <Button
-          variant="success"
-          onClick={() => {
-            
-          }}
-        >
-          Subir Archivos
-        </Button>
-        </Col>
       </Form>
     </Container>
   );
